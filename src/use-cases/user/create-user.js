52cd11f55af1258698d3c88ea1b2226d4209b63c
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-07-16 22:23:25 -0300

    refactor: move user use-cases to its own folder

--- src/use-cases/user/create-user.js
+++ src/use-cases/user/create-user.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import bcrpty from 'bcrypt';
-import { EmailAlreadyInUseError } from '../errors/index.js';
+import { EmailAlreadyInUseError } from '../../errors/index.js';
 
 export class CreateUserUseCase {
     constructor(createUserRepository, getUserByEmailRepository) {
