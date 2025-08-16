bb3fb89d6e1caff93e4c7f3d6390ab99ce30648a
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-08-16 12:15:33 -0300

    feat: add DeleteTransactionController

---
+++ src/controllers/transaction/delete-transaction.js
@@ -0,0 +1,31 @@
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+} from '../helpers/index.js';
+
+export class deleteTransactionController {
+    constructor(deleteTransactionUseCase) {
+        this.deleteTransactionUseCase = deleteTransactionUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const transactionId = httpRequest.params.transactionId;
+            const idIsValid = checkIfIdIsValid(transactionId);
+
+            if (idIsValid) {
+                return invalidIdResponse();
+            }
+
+            const transaction =
+                await this.deleteTransactionUseCase.execute(transactionId);
+            return ok(transaction);
+        } catch (error) {
+            console.error(error);
+
+            return serverError();
+        }
+    }
+}
