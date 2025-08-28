add9c0e928a8ee05930276dc32ebadaf0a453f9a
Bruno-Camargo-V3ktor <v3ktoryt@gmail.com>
2025-08-28 19:13:46 -0300

    feat: add prisma in CreateUserRepository

--- src/repositories/postgres/user/create-user.js
+++ src/repositories/postgres/user/create-user.js
@@ -1,23 +1,17 @@
-import { PostgresHelper } from '../../../db/postgres/client.js';
+import prisma from '../../../../prisma/prisma.js';
 
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        await PostgresHelper.query(
-            'INSERT INTO users (id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
-            [
-                createUserParams.id,
-                createUserParams.firstName,
-                createUserParams.lastName,
-                createUserParams.email,
-                createUserParams.password,
-            ],
-        );
+        const user = await prisma.user.create({
+            data: {
+                id: createUserParams.id,
+                email: createUserParams.email,
+                fisrt_name: createUserParams.firstName,
+                last_name: createUserParams.lastName,
+                password: createUserParams.password,
+            },
+        });
 
-        const createdUser = await PostgresHelper.query(
-            'SELECT * FROM USERS WHERE id = $1',
-            [createUserParams.id],
-        );
-
-        return createdUser[0];
+        return user;
     }
 }
